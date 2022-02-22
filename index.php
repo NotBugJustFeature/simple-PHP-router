3f77006d389d6870b6b52f48183b4810d5a8f16f
notbugjustfeature <dreamhun@protonmail.com>
2022-02-22 17:21:27 +0100

    V2 - Customizeable param type

--- index.php
+++ index.php
@@ -4,20 +4,25 @@
     $apiDir = "api";
 
     $routes = [
-        ["/",               "{$publicDir}/index.php"],  
-        ["/about",          "{$publicDir}/about.php"],
-        ["/?/:parameter",   "{$publicDir}/user.php",     ["u","user"]],
+        ["/",                   "{$publicDir}/index.php"],  
+        ["/about",              "{$publicDir}/about.php"],
+        ["/user/:parameter",    "{$publicDir}/user.php"],
     ];
 
-    $router = new Router($routes, $_SERVER['REQUEST_URI']);
+    $router = new Router($routes, $_SERVER['REQUEST_URI'], Router::BOTH);
     $_PARAMS = $router->execRouting();
     require $router->getDest();
+    //echo $router->getDest()."<br>";
+    //print_r($_PARAMS);
 
     /*
+        Router(routeList, current route, paramlist type = [ARRAY, ASSOC, BOTH])
         $routes = [
-            ["urlDest",             "dirDest"],  
-            ["urlDest/:parameter",  "dirDest"], will be stored in the $_PARAMS array  
-            ["/?/:parameter",       "{$publicDir}/user.php",     ["u","user"]], In parameter 3 you can create a alias, and ? shows the position of the alias.
+            ["urlDest",                 "dirDest"],  
+            ["urlDest/:parameter",      "dirDest"], will be stored in the $_PARAMS array
+            ["urlDest/:parameter",      "{$publicDir}/user.php",     
         ]; 
     */
+    
+
 ?>
--- router.class.php
+++ router.class.php
@@ -4,27 +4,30 @@
         private $route;
         private $params;
         private $dest;
+        private $mode;
 
-        function __construct($routes, $route){
-            
-            $this->route  = $route;
-            $res = [];
-            foreach($routes as $index => $rs)
-                if(isset($rs[2]))
-                    for($i = 0; $i < count($rs[2]); $i++)
-                        array_push($res, [str_replace("?", $rs[2][$i],$rs[0]), $rs[1]] );
-                else array_push($res, $rs);
-            
-            $this->routes = $res;
+        const BOTH = 3;
+        const ASSOC = 2;
+        const ARRAY = 1;
+
+        function __construct($routes, $route, $mode = self::ARRAY){
+            $this->route    = $route;
+            $this->mode     = $mode;
+            $this->routes   = $routes;
+            $res            = [];
         }
         function getDest(){
             return $this->dest;
         }
         function execRouting(){
             $match = -1;
-            $rexp = explode("/",$this->route);
+            if(strpos($this->route, "?") != false) {
+                $spl = explode("?", $this->route);
+                $rexp = explode("/",$spl[0]);
+            }else $rexp = explode("/",$this->route);
 
             unset($rexp[0]);
+
             foreach($this->routes as $index => $rs){
                 $rsexp = explode("/",$rs[0]);
                 unset($rsexp[0]);
@@ -34,8 +37,9 @@
                 if(count($rsexp) == count($rexp)){
                     for($i = 1; $i < count($rexp)+1; $i++){
 
-                        if(strlen($rsexp[$i]) > 0 && $rsexp[$i]{0} == ":"){
-                            array_push($this->params, $rexp[$i]);    
+                        if(strlen($rsexp[$i]) > 0 && $rsexp[$i][0] == ":"){
+                            if(($this->mode & 1)) array_push($this->params, $rexp[$i]);
+                            if(($this->mode>>1 & 1)) $this->params = array_merge($this->params, [substr($rsexp[$i], 1) => $rexp[$i]]);
                         }else{
                             if( $rsexp[$i] != $rexp[$i] ){
                                 $success = false;
